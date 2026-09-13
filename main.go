020b29b46efec8c79d2c584a6054fdac6a839f1f
devender.p <devender.padi@gmail.com>
2026-09-13 16:51:25 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	uri := "mongodb+srv://devender:Mongodb@cluster0.gcq5qxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to Atlas!")
+
+	coll := client.Database("practiceDB").Collection("items")
+	res, err := coll.InsertOne(ctx, map[string]any{"name": "test", "createdAt": time.Now()})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted ID:", res.InsertedID)
+}
