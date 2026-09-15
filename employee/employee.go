5af79ceca2adaae18a3f7a97ba1deb6404fdaf2a
devender.p <devender.padi@gmail.com>
2026-09-15 17:07:42 +0530

    modified with CRUD operations

--- employee/employee.go
+++ employee/employee.go
@@ -3,15 +3,17 @@ package employee
 import (
 	"context"
 
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type Employee3 struct {
-	Name string `bson:"name"`
-	Dept string `bson:"dept"`
+	ID   bson.ObjectID `bson:"_id,omitempty"`
+	Name string        `bson:"name"`
+	Dept string        `bson:"dept"`
 }
 
-func NewEmployee3(name string, dept string) Employee3 {
+func NewEmployee(name string, dept string) Employee3 {
 	return Employee3{Name: name, Dept: dept}
 }
 
@@ -22,4 +24,77 @@ func InsertEmployee(ctx context.Context, coll *mongo.Collection, emp Employee3) 
 		return nil, err
 	}
 	return res.InsertedID, nil
+}
+
+// GetEmployeeByID finds a single employee by its ObjectID.
+func GetEmployeeByID(ctx context.Context, coll *mongo.Collection, id bson.ObjectID) (*Employee3, error) {
+	var emp Employee3
+	err := coll.FindOne(ctx, bson.M{"_id": id}).Decode(&emp)
+	if err != nil {
+		return nil, err
+	}
+	return &emp, nil
+}
+
+// GetAllEmployees returns every employee document in the collection.
+func GetAllEmployees(ctx context.Context, coll *mongo.Collection) ([]Employee3, error) {
+	cursor, err := coll.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var employees []Employee3
+	if err := cursor.All(ctx, &employees); err != nil {
+		return nil, err
+	}
+	return employees, nil
+}
+
+// GetEmployeesByDept finds all employees in a given department.
+func GetEmployeesByDept(ctx context.Context, coll *mongo.Collection, dept string) ([]Employee3, error) {
+	cursor, err := coll.Find(ctx, bson.M{"dept": dept})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var employees []Employee3
+	if err := cursor.All(ctx, &employees); err != nil {
+		return nil, err
+	}
+	return employees, nil
+}
+
+// UpdateEmployeeDept updates the department field for a given employee ID.
+// Returns the number of documents modified.
+func UpdateEmployeeDept(ctx context.Context, coll *mongo.Collection, id bson.ObjectID, newDept string) (int64, error) {
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"dept": newDept}}
+
+	res, err := coll.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return 0, err
+	}
+	return res.ModifiedCount, nil
+}
+
+// DeleteEmployee removes a single employee by ID.
+// Returns the number of documents deleted (0 or 1).
+func DeleteEmployee(ctx context.Context, coll *mongo.Collection, id bson.ObjectID) (int64, error) {
+	res, err := coll.DeleteOne(ctx, bson.M{"_id": id})
+	if err != nil {
+		return 0, err
+	}
+	return res.DeletedCount, nil
+}
+
+// DeleteEmployeesByDept removes all employees in a given department.
+// Returns the number of documents deleted.
+func DeleteEmployeesByDept(ctx context.Context, coll *mongo.Collection, dept string) (int64, error) {
+	res, err := coll.DeleteMany(ctx, bson.M{"dept": dept})
+	if err != nil {
+		return 0, err
+	}
+	return res.DeletedCount, nil
 }
--- main.go
+++ main.go
@@ -7,13 +7,12 @@ import (
 	"time"
 
 	"github.com/devender81/mongodb/employee"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 func main() {
-	emp3 := employee.NewEmployee3("adv10", "cloud engineer")
-	fmt.Println(emp3)
 
 	uri := "mongodb+srv://devender:Devender@cluster0.gcq5qxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 	//uri := "mongodb://localhost:27017"    //connecting mongodb through docker
@@ -33,9 +32,69 @@ func main() {
 
 	coll := client.Database("practiceDB").Collection("employees")
 
-	id, err := employee.InsertEmployee(ctx, coll, emp3)
+	// ---------- CREATE ----------
+	emp := employee.NewEmployee("adv1", "devops")
+	insertedID, err := employee.InsertEmployee(ctx, coll, emp)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Inserted employee with ID:", id)
+	fmt.Println("Inserted employee with ID:", insertedID)
+
+	// Cast the returned ID (interface{}) into a bson.ObjectID so we can use it below.
+	objID, ok := insertedID.(bson.ObjectID)
+	if !ok {
+		log.Fatal("could not cast inserted ID to bson.ObjectID")
+	}
+
+	// Insert one more so Read-all/Read-by-dept have more than one document to show.
+	emp2 := employee.NewEmployee("adv2", "sales")
+	if _, err := employee.InsertEmployee(ctx, coll, emp2); err != nil {
+		log.Fatal(err)
+	}
+
+	// ---------- READ ----------
+	fetched, err := employee.GetEmployeeByID(ctx, coll, objID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Fetched by ID:", *fetched)
+
+	all, err := employee.GetAllEmployees(ctx, coll)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("All employees:", all)
+
+	devopsFolks, err := employee.GetEmployeesByDept(ctx, coll, "devops")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Devops employees:", devopsFolks)
+
+	// ---------- UPDATE ----------
+	modifiedCount, err := employee.UpdateEmployeeDept(ctx, coll, objID, "platform-engineering")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Documents modified:", modifiedCount)
+
+	updated, err := employee.GetEmployeeByID(ctx, coll, objID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("After update:", *updated)
+
+	// ---------- DELETE ----------
+	deletedCount, err := employee.DeleteEmployee(ctx, coll, objID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Documents deleted:", deletedCount)
+
+	remaining, err := employee.GetAllEmployees(ctx, coll)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Remaining employees:", remaining)
+
 }
